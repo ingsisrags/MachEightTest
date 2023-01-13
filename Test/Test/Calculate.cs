fc5648266be74fc5d439cfef3e721de0a5cd85cc
Ricardo Gomez <ricardo.gomez@itglobers.com>
2023-01-13 17:13:38 -0500

    add test

---
+++ Test/Test/Calculate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Calculate : ICalculate
+    {
+        private int[] list;
+        private int sum;
+        public Calculate(int[] _list, int _sum)
+        {
+            list = _list;
+            sum = _sum;
+
+        }
+        public Task Cal()
+        {
+           return findSumPar(list, sum, list[0], 1);
+        }
+
+        public Task findSumPar(int[] list, int sum, int x, int y)
+        {
+            if (x <= list.Length - 1 && y <= list.Length - 1)
+            {
+                if (y == list.Length - 1)
+                {
+                    Thread thready = new Thread(async() => await findSumPar(list, sum, x + 1, 0));
+                    thready.Start();
+
+                }
+                else
+                if ((list[x] + list[y] == sum) && x != y)
+                {
+                    Console.WriteLine($"{list[x]},{list[y]} ");
+                }
+                Thread threadx = new Thread(async () => await findSumPar(list, sum, x, y + 1));
+                threadx.Start();
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Test/Test/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualBasic;
+using System.Runtime.CompilerServices;
+using Test;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+            Console.WriteLine("Ingres a list of numbers separated with coma(,)");
+
+            var entrance = Console.ReadLine();
+
+            Console.WriteLine("Ingres a number for the sum");
+
+            var numForSum = Console.ReadLine();
+            var arrayString = entrance?.Split(",");
+            var listInt = Array.ConvertAll(arrayString, int.Parse);
+            ICalculate _calculate = new Calculate( listInt , int.Parse(numForSum));
+            _calculate.Cal();
+
+            Console.ReadKey();
+
+
+    }
+}
